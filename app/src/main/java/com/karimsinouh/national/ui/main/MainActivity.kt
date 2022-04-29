b38d6e08ccaf3c581145296b22d14c60f21b67c3
karim <sinouhkar@gmail.com>
2022-04-29 00:05:46 +0000

    hide bottom bar

--- app/src/main/java/com/karimsinouh/national/ui/main/MainActivity.kt
+++ app/src/main/java/com/karimsinouh/national/ui/main/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.karimsinouh.national.ui.theme.NationalTheme
 import com.karimsinouh.national.util.Screens
@@ -43,17 +44,28 @@ class MainActivity : ComponentActivity() {
                 topBar = { MainTopBar() },
                 content={ MainNavHost() },
                 bottomBar = {
-                    MainBottomBar(
-                        selectedRoute = nav.currentDestination?.route ?: Screens.Home.route,
-                        onNavigation = {
-                            nav.navigate(it.route){
-                                popUpTo(Screens.Home.route)
+                    if(shouldShowBottomBar())
+                        MainBottomBar(
+                            selectedRoute = nav.currentDestination?.route ?: Screens.Home.route,
+                            onNavigation = {
+                                nav.navigate(it.route){
+                                    popUpTo(Screens.Home.route)
+                                }
                             }
-                        }
-                    )
+                        )
                 }
             )
 
         }
+    }
+
+    @Composable
+    private fun shouldShowBottomBar():Boolean{
+        val backstack=nav.currentBackStackEntryAsState()
+        Screens.Get.bottomNavigationItems.forEach {
+            if (it.route==backstack.value?.destination?.route)
+                return true
+        }
+        return false
     }
 }
